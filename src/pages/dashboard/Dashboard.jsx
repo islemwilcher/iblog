233e9437607b34df2579a188935ce180d2e9fa55
islemwilcher <islemadobe6@gmail.com>
2021-10-07 13:22:50 -0700

    fixing the sign in issue

--- src/pages/dashboard/Dashboard.jsx
+++ src/pages/dashboard/Dashboard.jsx
@@ -3,15 +3,6 @@ const Dashboard = () => {
     return (
         <div>
             <h1>dashboard</h1>
-            <h1>dashboard</h1>
-            <h1>dashboard</h1>
-            <h1>dashboard</h1>
-            <h1>dashboard</h1>
-            <h1>dashboard</h1>
-            <h1>dashboard</h1>
-            <h1>dashboard</h1>
-            <h1>dashboard</h1>
-            <h1>dashboard</h1>
         </div>
     )
 }
--- src/pages/signin/signin.jsx
+++ src/pages/signin/signin.jsx
@@ -9,15 +9,16 @@ import useStyles from './styles'
 
 const Signin = () => {
 
-    const [form, setForm] = useState({email: '', password:''})
+    const [form, setForm] = useState({ email: '', password:'' })
     const dispatch = useDispatch()
     const history = useHistory()
     const classes = useStyles()
 
     const handleSubmit = (e) => {
         e.preventDefault()
-    
+        console.log(form)
         dispatch(signin(form, history))
+        
     }
 
     const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value })
@@ -28,8 +29,8 @@ const Signin = () => {
                 <Typography component="h1" variant="h5">Sign in</Typography>
                 <form className={classes.form} onSubmit={handleSubmit}>
                 <Grid container spacing={2}>
-                    <TextField className={classes.text} variant='outlined' name="email" label="Email Address" handleChange={handleChange} type="email" fullWidth />
-                    <TextField className={classes.text} variant='outlined' name="password" label="Password" handleChange={handleChange} type='password' fullWidth />
+                    <TextField className={classes.text} variant='outlined' name="email" label="Email Address" onChange={handleChange} type="email" fullWidth />
+                    <TextField className={classes.text} variant='outlined' name="password" label="Password" onChange={handleChange} type='password' fullWidth />
                 </Grid>
                 <Button type="submit" fullWidth variant="contained" color="primary" className={classes.submit} >
                     Sign In
