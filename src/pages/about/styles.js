4de0d58ad1dd09d3972d1b05658557caa8c9eadc
islemwilcher <islemadobe6@gmail.com>
2021-10-08 12:06:27 -0700

    fixing all posts functions and adding social media icons to the about page.

--- src/pages/about/styles.js
+++ src/pages/about/styles.js
@@ -12,5 +12,27 @@ export default makeStyles({
   },
   paragraph: {
     fontWeight: '400',
-  }
+  },
+  socialmedia: {
+    margin: '35px auto',
+    width: '300px',
+    display: 'flex',
+    justifyContent: 'space-between',
+  },
+  insta: {
+    fontSize: '32px',
+    color: '#e045e6',
+  },
+  linkin: {
+    fontSize: '35px',
+    color: '#4d7fff',
+  },
+  twit: {
+    fontSize: '35px',
+    color: '#2c93c7',
+  },
+  github: {
+    fontSize: '32px',
+    color: '#000',
+  },
 });
--- src/pages/all posts/AllPosts.jsx
+++ src/pages/all posts/AllPosts.jsx
@@ -1,9 +1,21 @@
 
+import { useState, useEffect } from 'react'
+import { useDispatch } from 'react-redux'
+import { getPosts } from '../../actions/posts'
+
 import { Container } from '@material-ui/core'
 import Posts from '../admin/manageposts/posts/posts'
 import useStyles from './styles'
 const AllPosts = () => {
+
+    const dispatch = useDispatch();
     const classes = useStyles()
+
+    useEffect(() => {
+        dispatch(getPosts());
+      }, [dispatch]);
+    
+    
     return (
         <Container className={classes.container}>
             <Posts />
